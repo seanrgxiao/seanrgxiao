06b6af2194760c7d5ba368e35a17cc60cb6f7c51
seanrgxiao <sean7.xiao@gmail.com>
2025-04-18 09:28:20 +0000

    Change to asg

--- terraform/01-give-it-a-try.tf
+++ terraform/01-give-it-a-try.tf
@@ -2,20 +2,28 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-01938df366ac2d954"
+resource "aws_launch_configuration" "example" {
+  image_id           = "ami-01938df366ac2d954"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
   user_data     = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size = 2
+  max_size = 10
 
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 resource "aws_security_group" "instance" {
