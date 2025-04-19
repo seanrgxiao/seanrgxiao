94dd37e89de515813697857a3aa5dffa8be5aa92
seanrgxiao <sean7.xiao@gmail.com>
2025-04-19 05:29:55 +0000

    Using gpt example to test correct timezone

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-southeast-1a"
 }
 
 resource "aws_internet_gateway" "gw" {
