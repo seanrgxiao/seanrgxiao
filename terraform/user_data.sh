ec3fbcfda734a9ec9a373f70d8415cfd10da7655
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 05:58:11 +0000

    Move var to separate

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -5,7 +5,7 @@ sudo echo 'Hello, test3' >> /tmp/user_data.log
 exec > /var/log/user-data.log 2>&1
 set -x
 echo 'Hello, test2' >> /tmp/user_data.log
-echo ${var.server_port} >> /tmp/user_data.log
+echo "${var.server_port}" >> /tmp/user_data.log
 # echo "Hello, World" > user-data.log
 echo "Hello, World" > index.html
 # nohup busybox httpd -f -p 8080 &
