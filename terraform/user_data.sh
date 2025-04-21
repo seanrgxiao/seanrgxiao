05880a8741a71a0aa00328841670a333c0f427ca
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 03:35:10 +0000

    Update user_data.sh with echo

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo 'Hello, test' > /tmp/user_data.log
 
 exec > /var/log/user-data.log 2>&1
 set -x
-echo "Hello, World" > user-data.log
-echo "Hello, World" > /tmp/index.html
-nohup busybox httpd -f -p ${var.server_port} &
+echo 'Hello, test2' > /tmp/user_data.log
+
+# echo "Hello, World" > user-data.log
+# echo "Hello, World" > /tmp/index.html
+# nohup busybox httpd -f -p ${var.server_port} &
