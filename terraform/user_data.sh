a66beddac0e672cf916fb6a8888c95ca489ad4ec
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 04:34:53 +0000

    Updated user_data.sh with server_port print

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -5,7 +5,7 @@ sudo echo 'Hello, test3' >> /tmp/user_data.log
 exec > /var/log/user-data.log 2>&1
 set -x
 echo 'Hello, test2' >> /tmp/user_data.log
-
+echo ${var.server_port} >> /tmp/user_data.log
 # echo "Hello, World" > user-data.log
 # echo "Hello, World" > /tmp/index.html
 nohup busybox httpd -f -p ${var.server_port} &
