cd59f4954278d865679b770dee24964ffb729f94
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 07:46:15 +0000

    update dynamodb name

--- terraform/main.tf
+++ terraform/main.tf
@@ -164,7 +164,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
+  name         = "terraform-up-and-running-locks-by-seanrgxiao"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
