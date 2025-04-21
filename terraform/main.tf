26ed7474bc81d289703dc844dc3b3f9dcc409d4e
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 03:30:48 +0000

    Add user_data.sh config in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,7 @@ resource "aws_launch_template" "example" {
   image_id           = "ami-01938df366ac2d954"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = base64encode(file("user_data.sh"))
 
   lifecycle {
     create_before_destroy = true
