52cf0aeafd78ff1c042e20df0b8c9370dc9025ee
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 07:28:15 +0000

    s3 versioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,4 +139,10 @@ resource "aws_s3_bucket" "terraform_state" {
     prevent_destroy = true
   }
 }
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
