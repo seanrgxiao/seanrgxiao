d43f6efa22a332fbc3091834e64c43d3b9c4f75c
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 08:52:06 +0000

    recovery to put backend with main.tf together.

--- terraform/main.tf
+++ terraform/main.tf
@@ -176,12 +176,12 @@ resource "aws_dynamodb_table" "terraform_locks" {
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    # bucket         = "terraform-up-and-running-state-by-seanrgxiao"
+    bucket         = "terraform-up-and-running-state-by-seanrgxiao"
     key            = "global/s3/terraform.tfstate"
-    # region         = "ap-southeast-1"
+    region         = "ap-southeast-1"
 
     # Replace this with your DynamoDB table name!
-    # dynamodb_table = "terraform-up-and-running-locks-by-seanrgxiao"
-    # encrypt        = true
+    dynamodb_table = "terraform-up-and-running-locks-by-seanrgxiao"
+    encrypt        = true
   }
 }
