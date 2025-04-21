a305038d4b78f402ba086045097242cc0245bc3e
seanrgxiao <sean7.xiao@gmail.com>
2025-04-21 04:29:42 +0000

    Updated user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo 'Hello, test' > /tmp/user_data.log
+echo 'Hello, test' >> /tmp/user_data.log
+sudo echo 'Hello, test3' >> /tmp/user_data.log
 
 exec > /var/log/user-data.log 2>&1
 set -x
-echo 'Hello, test2' > /tmp/user_data.log
+echo 'Hello, test2' >> /tmp/user_data.log
 
 # echo "Hello, World" > user-data.log
 # echo "Hello, World" > /tmp/index.html
-# nohup busybox httpd -f -p ${var.server_port} &
+nohup busybox httpd -f -p ${var.server_port} &
