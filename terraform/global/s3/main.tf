502b6ba95aa38f46d4483a1e0571483a69d6e34f
seanrgxiao <sean7.xiao@gmail.com>
2025-04-22 06:35:51 +0000

    separate backend config file.

--- terraform/global/s3/main.tf
+++ terraform/global/s3/main.tf
@@ -40,16 +40,3 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-by-seanrgxiao"
-    # key            = "global/s3/terraform.tfstate"
-    key = "workspaces-example/terraform.tfstate"
-    region         = "ap-southeast-1"
-
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks-by-seanrgxiao"
-    encrypt        = true
-  }
-}
