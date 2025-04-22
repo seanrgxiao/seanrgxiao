a0bdaaa62dc01bf4e82dfe500716c8a959fa5883
seanrgxiao <sean7.xiao@gmail.com>
2025-04-22 02:06:02 +0000

    Add single vm and  update backend configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -177,11 +177,16 @@ terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "terraform-up-and-running-state-by-seanrgxiao"
-    key            = "global/s3/terraform.tfstate"
+    # key            = "global/s3/terraform.tfstate"
+    key = "workspaces-example/terraform.tfstate"
     region         = "ap-southeast-1"
 
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks-by-seanrgxiao"
     encrypt        = true
   }
+}
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 }
