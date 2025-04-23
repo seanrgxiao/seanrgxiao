e32be8e9abdc48f0f3ef792859ecdc78235d52ca
seanrgxiao <sean7.xiao@gmail.com>
2025-04-23 01:54:21 +0000

    Expose the lambda function to the public internet

--- terraform/stage/services/lambda-demo/main.tf
+++ terraform/stage/services/lambda-demo/main.tf
@@ -36,3 +36,36 @@ resource "aws_lambda_function" "hello_lambda" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.9"
 }
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "lambda_http_api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.hello_lambda.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
--- terraform/stage/services/lambda-demo/outputs.tf
+++ terraform/stage/services/lambda-demo/outputs.tf
@@ -1,3 +1,6 @@
 output "lambda_function_name" {
   value = aws_lambda_function.hello_lambda.function_name
 }
+output "lambda_api_url" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
