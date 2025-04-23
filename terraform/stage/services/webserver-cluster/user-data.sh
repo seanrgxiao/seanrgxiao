95e89eabfe616a93d554c4ea8953de57d8af78f6
seanrgxiao <sean7.xiao@gmail.com>
2025-04-23 09:02:41 +0000

    Update user-data.sh

--- terraform/stage/services/webserver-cluster/user-data.sh
+++ terraform/stage/services/webserver-cluster/user-data.sh
@@ -2,9 +2,12 @@
 
 exec > /var/log/user-data.log 2>&1
 set -x
-
+echo "<pre>" >> index.html
 echo "Hello, World" >> index.html
 echo "${db_address}" >> index.html
 echo "${db_port}" >> index.html
 echo "${server_port}" >> index.html
+echo "</pre>" >> index.html
+
+
 nohup busybox httpd -f -p ${server_port} &
