fbe0ee1b1b410eda441079a2e9c8a928ae6f9cb9
seanrgxiao <sean7.xiao@gmail.com>
2025-04-25 09:16:47 +0000

    Comment out db_addr and db_port in modules/main.tf

--- terraform/modules/services/webserver-cluster/main.tf
+++ terraform/modules/services/webserver-cluster/main.tf
@@ -14,8 +14,8 @@ resource "aws_launch_template" "example" {
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = base64encode(templatefile("user-data.sh", {
     server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
+    # db_address  = data.terraform_remote_state.db.outputs.address
+    # db_port     = data.terraform_remote_state.db.outputs.port
   }))
 
   lifecycle {
