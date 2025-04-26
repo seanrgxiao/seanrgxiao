f740fd4b37e92367e8baf308a6ba7ab16f248314
seanrgxiao <sean7.xiao@gmail.com>
2025-04-26 04:00:32 +0000

    Comment out db part in user-data

--- terraform/modules/services/webserver-cluster/user-data.sh
+++ terraform/modules/services/webserver-cluster/user-data.sh
@@ -4,8 +4,8 @@ exec > /var/log/user-data.log 2>&1
 set -x
 echo "<pre>" >> index.html
 echo "Hello, World" >> index.html
-echo "${db_address}" >> index.html
-echo "${db_port}" >> index.html
+# echo "${db_address}" >> index.html
+# echo "${db_port}" >> index.html
 echo "${server_port}" >> index.html
 echo "</pre>" >> index.html
 
