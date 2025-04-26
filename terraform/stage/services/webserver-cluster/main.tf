e22bd864bbaffc2282ea7294b92e076450c6c150
seanrgxiao <sean7.xiao@gmail.com>
2025-04-26 06:38:14 +0000

    Update bucket name for state file

--- terraform/stage/services/webserver-cluster/main.tf
+++ terraform/stage/services/webserver-cluster/main.tf
@@ -6,7 +6,7 @@ module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
   cluster_name           = "webserver-stage"
-  db_remote_state_bucket = "terraform-up-and-running-state-by-seanrgxiao-stage"
+  db_remote_state_bucket = "terraform-up-and-running-state-by-seanrgxiao"
   db_remote_state_key    = "stage/services/webserver-cluster/terraform-webserver.tfstate"
 
   instance_type = "t2.micro"
