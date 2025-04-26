d8a916ae5c0918ccc3d21da7aa3f914d6814d874
seanrgxiao <sean7.xiao@gmail.com>
2025-04-26 03:20:29 +0000

    Update file to path.module 2

--- terraform/modules/services/webserver-cluster/main.tf
+++ terraform/modules/services/webserver-cluster/main.tf
@@ -13,7 +13,7 @@ resource "aws_launch_template" "example" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
   # user_data = base64encode(templatefile("user-data.sh", {
-    user_data = base64encode(path.module("user-data.sh", {
+    user_data = base64encode(templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
     # db_address  = data.terraform_remote_state.db.outputs.address
     # db_port     = data.terraform_remote_state.db.outputs.port
