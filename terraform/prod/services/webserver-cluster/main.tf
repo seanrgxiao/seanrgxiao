3648ff6becc26b623902bffc72d0322ef61e51d5
seanrgxiao <sean7.xiao@gmail.com>
2025-04-30 08:26:14 +0000

    Update size of asg

--- terraform/prod/services/webserver-cluster/main.tf
+++ terraform/prod/services/webserver-cluster/main.tf
@@ -27,7 +27,7 @@ resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
   scheduled_action_name = "scale-out-during-business-hours"
   min_size              = 2
   max_size              = 10
-  desired_capacity      = 10
+  desired_capacity      = 2
   recurrence            = "30 16 * * *"
 
   autoscaling_group_name = module.webserver_cluster.asg_name
@@ -37,7 +37,7 @@ resource "aws_autoscaling_schedule" "scale_in_at_night" {
   scheduled_action_name = "scale-in-at-night"
   min_size              = 5
   max_size              = 10
-  desired_capacity      = 2
+  desired_capacity      = 5
   recurrence            = "50 16 * * *"
 
   autoscaling_group_name = module.webserver_cluster.asg_name
