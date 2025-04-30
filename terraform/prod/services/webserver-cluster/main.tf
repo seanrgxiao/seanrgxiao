b7fa5a9245541c2464446aed7ab0c314fa0b4ac7
seanrgxiao <sean7.xiao@gmail.com>
2025-04-30 03:35:57 +0000

    Keep s3 bucket name

--- terraform/prod/services/webserver-cluster/main.tf
+++ terraform/prod/services/webserver-cluster/main.tf
@@ -6,7 +6,7 @@ module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
 
   cluster_name           = "webserver-prod"
-  db_remote_state_bucket = "terraform-up-and-running-state-by-seanrgxiao-prod"
+  db_remote_state_bucket = "terraform-up-and-running-state-by-seanrgxiao"
   db_remote_state_key    = "prod/services/webserver-cluster/terraform-webserver.tfstate"
 
   instance_type = "m4.large"
@@ -28,7 +28,7 @@ resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
   min_size              = 2
   max_size              = 10
   desired_capacity      = 10
-  recurrence            = "35 11 * * *"
+  recurrence            = "40 11 * * *"
 
   autoscaling_group_name = module.webserver_cluster.asg_name
 }
