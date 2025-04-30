4e48ede725face3530b728cfc04741c7d43b1b81
seanrgxiao <sean7.xiao@gmail.com>
2025-04-30 09:11:53 +0000

    Add dynamodb_table for lock

--- terraform/prod/services/webserver-cluster/backend.tf
+++ terraform/prod/services/webserver-cluster/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "terraform-up-and-running-state-by-seanrgxiao"
     key    = "prod/services/webserver-cluster/terraform-webserver.tfstate"
     region = "ap-southeast-1"
+    dynamodb_table = "terraform-up-and-running-locks-by-seanrgxiao"
+    encrypt = true    
   }
 }
