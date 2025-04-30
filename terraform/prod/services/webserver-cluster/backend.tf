3ecb01705f09558d65ac78d9a89bc18832a2035d
seanrgxiao <sean7.xiao@gmail.com>
2025-04-30 08:11:16 +0000

    Comment data-backend out

--- terraform/prod/services/webserver-cluster/backend.tf
+++ terraform/prod/services/webserver-cluster/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-by-seanrgxiao"
-    key    = "prod/services/webserver-cluster/terraform-webserver.tfstate"
-    region = "ap-southeast-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-up-and-running-state-by-seanrgxiao"
+#     key    = "prod/services/webserver-cluster/terraform-webserver.tfstate"
+#     region = "ap-southeast-1"
+#   }
+# }
