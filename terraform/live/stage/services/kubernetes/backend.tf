e00231ef01f0ed4b141a0cd558449aa50788c575
seanrgxiao <sean7.xiao@gmail.com>
2025-05-20 08:34:01 +0000

    Add remote backend

---
+++ terraform/live/stage/services/kubernetes/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io" # 通常是这个，除非您有私有部署
+    organization = "batangas" # 替换为您的组织名称
+    workspaces {
+      name = "seanrgxiao" # 替换为您的工作空间名称
+    }
+  }
+}
