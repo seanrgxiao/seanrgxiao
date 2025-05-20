28b4129c6c0ede91ea229b4bdcdfb3f6959afa36
seanrgxiao <sean7.xiao@gmail.com>
2025-05-20 08:45:35 +0000

    Update new workspace

--- terraform/live/stage/services/kubernetes/backend.tf
+++ terraform/live/stage/services/kubernetes/backend.tf
@@ -3,7 +3,7 @@ terraform {
     hostname     = "app.terraform.io" # 通常是这个，除非您有私有部署
     organization = "batangas" # 替换为您的组织名称
     workspaces {
-      name = "seanrgxiao" # 替换为您的工作空间名称
+      name = "stage_kubernetes" # 替换为您的工作空间名称
     }
   }
 }
