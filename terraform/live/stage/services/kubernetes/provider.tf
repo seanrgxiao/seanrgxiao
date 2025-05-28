22c723a2bd25b1fb69c988b886d6e8b5edd348f3
seanrgxiao <sean.rg.xiao-01@hkjc.org.hk>
2025-05-28 13:50:33 +0800

    Comment out all backend settings

--- terraform/live/stage/services/kubernetes/provider.tf
+++ terraform/live/stage/services/kubernetes/provider.tf
@@ -1,17 +1,17 @@
-terraform {
-  cloud {
-    organization = "batangas"
-    workspaces {
-      name = "kube"
-    }
-  }
-  
-#   required_version = ">= 1.1.0"
-  
-#   required_providers {
-#     random = {
-#       source  = "hashicorp/random"
-#       version = "3.3.2"
+# terraform {
+#   cloud {
+#     organization = "batangas"
+#     workspaces {
+#       name = "kube"
 #     }
 #   }
-}
+  
+# #   required_version = ">= 1.1.0"
+  
+# #   required_providers {
+# #     random = {
+# #       source  = "hashicorp/random"
+# #       version = "3.3.2"
+# #     }
+# #   }
+# }
