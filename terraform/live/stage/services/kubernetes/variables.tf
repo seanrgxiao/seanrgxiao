38789fe53ad8532dd0c1000848147b1f4fba9056
seanrgxiao <sean.rg.xiao-01@hkjc.org.hk>
2025-05-28 11:25:46 +0800

    Comment var aws id out

--- terraform/live/stage/services/kubernetes/variables.tf
+++ terraform/live/stage/services/kubernetes/variables.tf
@@ -1,8 +1,8 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "The AWS Access Key ID"
-  type        = string
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "The AWS Secret Access Key ID"
-  type        = string
-}
+# variable "AWS_ACCESS_KEY_ID" {
+#   description = "The AWS Access Key ID"
+#   type        = string
+# }
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   description = "The AWS Secret Access Key ID"
+#   type        = string
+# }
